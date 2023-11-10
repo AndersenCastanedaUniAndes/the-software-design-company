2f174799ff18595b337d3bc097901c8882a5c3da
Juan Carlos De Jesus <j.dejesus@uniandes.edu.co>
2023-11-10 00:02:25 -0500

    Agrega estructura inicial para cypress

---
+++ Entrega-semana-5/cypress/cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    projectId: "e2e-testing-with-cypress",
+    baseUrl: 'http://localhost:2369/ghost/',
+    pageLoadTimeout:120000,
+    defaultCommandTimeout:120000,
+    videosFolder:"./videos",
+    video: true,
+    env:{},
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
