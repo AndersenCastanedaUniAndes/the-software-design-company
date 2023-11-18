47a95bd9cf8d8801e6f52bb1384812dd51f121cc
ccpinzonh1 <cc.pinzonh1@uniandes.edu.co>
2023-11-18 17:41:52 -0500

    Add visual regression testing with ResembleJS

--- Entrega-semana-6/kraken/resemblejs.sh
+++ Entrega-semana-6/kraken/resemblejs.sh
@@ -1,4 +1,14 @@
+# Navigate to the directory where the package.json file is located
+cd ./resemblejs
+
+# Install npm dependencies
+npm install
+
+# Go back to kraken root folder
+cd ..
+
 # Run the index.js file located in the resemblejs folder
+
 node ./resemblejs/index.js
 
 # For each folder in the /resemblejs/results/comparison directory, open the report.html file in a browser
