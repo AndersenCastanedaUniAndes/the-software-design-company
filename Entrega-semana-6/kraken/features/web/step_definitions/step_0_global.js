966afb9d5a715420be15f2df1a076c216b6a357e
ccpinzonh1 <cc.pinzonh1@uniandes.edu.co>
2023-11-18 14:52:06 -0500

    Fix step definition and directory path in resemblejs index.js

--- Entrega-semana-6/kraken/features/web/step_definitions/step_0_global.js
+++ Entrega-semana-6/kraken/features/web/step_definitions/step_0_global.js
@@ -1,4 +1,10 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Before, When, Then } = require('@cucumber/cucumber');
+
+let tag = '';
+
+Before(function (scenario) {
+  tag = scenario?.gherkinDocument.feature.tags[0].name;
+});
 
 When('I enter email {kraken-string}', async function (email) {
   let usernameInput = await this.driver.$('input[name="identification"]');
@@ -10,10 +16,10 @@ When('I enter password {kraken-string}', async function (password) {
   return await passwordInput.setValue(password);
 });
 
-When('I click sign in button with selector {string}', async function(string) {
+When('I click sign in button with selector {string}', async function (string) {
   let singInButton = await this.driver.$(string);
   return await singInButton.click();
-})
+});
 
 When(
   'I enter title {kraken-string} into field with selector {string}',
@@ -28,6 +34,11 @@ When(
   'I click in the Continue, final review button with selector {string}',
   async function (selector) {
     let continueToFinalReviewButton = await this.driver.$(selector);
+    if (tag.includes('4.48.9')) {
+      continueToFinalReviewButton = await this.driver.$(
+        'button.gh-publishmenu-button'
+      );
+    }
     return await continueToFinalReviewButton.click();
   }
 );
@@ -35,6 +46,9 @@ When(
 When(
   'I go back to the editor section clicking the editor button with selector {string}',
   async function (selector) {
+    if (tag.includes('4.48.9')) {
+      return;
+    }
     let goBackToEditorButton = await this.driver.$(selector);
     return await goBackToEditorButton.click();
   }
@@ -44,6 +58,11 @@ When(
   'I click in the preview button with selector {string}',
   async function (selector) {
     let previewButton = await this.driver.$(selector);
+    if (tag.includes('4.48.9')) {
+      previewButton = await this.driver.$(
+        'button.gh-btn.gh-editor-preview-trigger'
+      );
+    }
     return await previewButton.click();
   }
 );
--- Entrega-semana-6/kraken/features/web/step_definitions/step_posts.js
+++ Entrega-semana-6/kraken/features/web/step_definitions/step_posts.js
@@ -3,15 +3,18 @@ const { expect } = require('chai');
 const chai = require('chai');
 const should = chai.should();
 
-let tag = ''
+let tag = '';
 
 Before(function (scenario) {
-  tag = scenario?.gherkinDocument.feature.tags[0].name
+  tag = scenario?.gherkinDocument.feature.tags[0].name;
 });
 
 When(
   'I go to the posts section with selector {string}',
   async function (string) {
+    if (tag.includes('4.48.9')) {
+      await this.driver.$('button.gh-alert-close').click();
+    }
     let element = await this.driver.$(string);
     return await element.click();
   }
@@ -20,17 +23,23 @@ When(
 When(
   'I click in the new post button with selector {string}',
   async function (string) {
-    console.log(tag)
-    let element = await this.driver.$(string)[1];
+    let element = await this.driver.$(string);
+    if (tag.includes('4.48.9')) {
+      element = await this.driver.$(
+        'a.ember-view.gh-btn.gh-btn-primary.view-actions-top-row'
+      );
+    }
     return await element.click();
   }
 );
-
 
 When(
   'I go back to the list of posts clicking the posts button with selector {string}',
   async function (string) {
     let element = await this.driver.$(string);
+    if (tag.includes('4.48.9')) {
+      element = await this.driver.$('a.ember-view.gh-editor-back-button');
+    }
     return await element.click();
   }
 );
@@ -39,9 +48,7 @@ Then(
   'I validate that the title of the post was modified successfully with {kraken-string}',
   async function (string) {
     let foundPage = false;
-    let elements = await this.driver.$$(
-      'li.gh-list-row.gh-posts-list-item.gh-post-list-plain-status'
-    );
+    let elements = await this.driver.$$('li.gh-list-row.gh-posts-list-item');
 
     for (const element of elements) {
       let name = await element.$('a').getText();
@@ -56,10 +63,16 @@ Then(
 
 When('I click in the Publish button', async function () {
   let element = await this.driver.$('button[data-test-button="publish-flow"]');
+  if (tag.includes('4.48.9')) {
+    element = await this.driver.$('div.gh-publishmenu-trigger');
+  }
   return await element.click();
 });
 
 When('I click in the Right now button', async function () {
+  if (tag.includes('4.48.9')) {
+    return;
+  }
   let element = await this.driver.$$('button.gh-publish-setting-title')[2];
   return await element.click();
 });
@@ -68,6 +81,9 @@ When(
   'I click in the shedule later button with selector {string}',
   async function (string) {
     let element = await this.driver.$$(string)[1];
+    if (tag.includes('4.48.9')) {
+      element = await this.driver.$$('div.gh-publishmenu-radio-button')[1];
+    }
     return await element.click();
   }
 );
@@ -76,12 +92,20 @@ When(
   'I click in the Publish post confirm button with selector {string}',
   async function (string) {
     let element = await this.driver.$(string);
+    if (tag.includes('4.48.9')) {
+      element = await this.driver.$(
+        'button.gh-btn.gh-btn-black.gh-btn-icon.ember-view'
+      );
+    }
     return await element.click();
   }
 );
 
 Then('I validate that the post was sheduled successfully', async function () {
   let element = await this.driver.$('a.gh-post-list-title span.scheduled');
+  if (tag.includes('4.48.9')) {
+    element = await this.driver.$('span.gh-content-status-scheduled');
+  }
   expect((await element.getText()).toLowerCase()).to.equal(
     'Scheduled'.toLowerCase()
   );
@@ -91,9 +115,7 @@ When(
   'I click the post in order to edit it {kraken-string}',
   async function (string) {
     let elementFound;
-    let elements = await this.driver.$$(
-      'li.gh-list-row.gh-posts-list-item.gh-post-list-plain-status'
-    );
+    let elements = await this.driver.$$('li.gh-list-row.gh-posts-list-item');
 
     for (const element of elements) {
       let name = await element.$('a').getText();
@@ -110,6 +132,10 @@ Then(
   'I update the post by clicking the Update button with selector {string}',
   async function (string) {
     let element = await this.driver.$(string);
+    if (tag.includes('4.48.9')) {
+      await this.driver.$('div.gh-publishmenu-trigger').click();
+      element = await this.driver.$('button.gh-publishmenu-button');
+    }
     return await element.click();
   }
 );
@@ -118,6 +144,9 @@ Then(
   'I see the preview of the post on a div with selector {string}',
   async function (string) {
     let element = await this.driver.$(string);
+    if (tag.includes('4.48.9')) {
+      element = await this.driver.$('div.fullscreen-modal-email-preview');
+    }
     should.exist(element);
   }
 );
@@ -125,6 +154,9 @@ Then(
 Then(
   'I click the analytics button associated to the post {kraken-string}',
   async function (string) {
+    if (tag.includes('4.48.9')) {
+      return;
+    }
     let elements = await this.driver.$$(
       'li.gh-list-row.gh-posts-list-item.gh-post-list-plain-status'
     );
@@ -145,6 +177,9 @@ Then(
 Then(
   'I check the analytics panel opens with selector {string}',
   async function (string) {
+    if (tag.includes('4.48.9')) {
+      return;
+    }
     let element = await this.driver.$(string);
     should.exist(element);
   }
