3d088fcde5d3e215f4c40e64d61d58ce4dea76c1
Juan Carlos De Jesus <j.dejesus@uniandes.edu.co>
2023-12-02 10:32:26 -0500

    Implementa filtros de miembros

--- Entrega-semana-6/playwright/tests/members/filtros-de-miembros.spec.js
+++ Entrega-semana-6/playwright/tests/members/filtros-de-miembros.spec.js
@@ -106,7 +106,7 @@ describe("Como usuario administrator quiero poder buscar por nombre cualquier mi
     dataPool = await loadDataPool();
   });
 
-  test.describe.only("Given un usuario autenticado en la aplicación", () => {
+  test.describe("Given un usuario autenticado en la aplicación", () => {
     test.beforeEach(async ({ page }) => {
       navigation = new NavigationPageObject(page);
       authorization = new AuthorizationPageObject(page);
@@ -189,3 +189,96 @@ describe("Como usuario administrator quiero poder buscar por nombre cualquier mi
     });
   });
 });
+
+describe("Como usuario administrator quiero poder buscar por email cualquier miembro activo usando los filtros", () => {
+  let navigation;
+  let authorization;
+  let members;
+  let dataPool;
+
+  test.beforeAll(async () => {
+    dataPool = await loadDataPool();
+  });
+
+  test.describe.only("Given un usuario autenticado en la aplicación", () => {
+    test.beforeEach(async ({ page }) => {
+      navigation = new NavigationPageObject(page);
+      authorization = new AuthorizationPageObject(page);
+      members = new MemberPageObject(page);
+
+      const user = process.env.USERNAMEGHOST;
+      const password = process.env.PASSWORD;
+
+      await navigation.goToRoot();
+
+      await authorization.fillOutUsername(user);
+      await navigation.screenshot("members-deletion");
+      await authorization.fillOutPassword(password);
+      await navigation.screenshot("members-deletion");
+      await authorization.submit();
+      await navigation.screenshot("members-deletion");
+    });
+
+    describe("And navega hacia la seccion de miembros", () => {
+      test.describe("And navega hacia la creación de un miembro", () => {
+        test.describe("When ingresar un nombre y un correo válido", () => {
+          test.describe("And el miembro es creado sastifactoriamene", () => {
+            test.describe("And navega nuevamente hacia la seccion de miembros", () => {
+              test.describe("And selecciona abrir la ventana de filtros", () => {
+                test.describe("When ingresar email de miembro creado", () => {
+                  test("Then miembro debe aparecer en la lista", async ({ page }) => {
+                    await navigation.clickOnMembersViewLink();
+                    await navigation.screenshot("members-deletion");
+
+                    await navigation.clickOnNewMemberViewLink();
+                    await navigation.screenshot("members-deletion");
+
+                    // GENERATES A NEW RAMDOM PERSON TO BE CREATED
+                    const member = generateValidRamdomMemberFromPool(dataPool);
+
+                    // FILL OUT THE NEW MEMBER FORM
+                    await members.fillOutName(member);
+                    await navigation.screenshot("members-deletion");
+                    await members.fillOutEmail(member);
+                    await navigation.screenshot("members-deletion");
+                    await members.clickOnSaveButtonByDataTest();
+                    await navigation.screenshot("members-deletion");
+                    await navigation.clickOnMembersViewLink();
+                    await navigation.screenshot("members-deletion");
+
+                    //CHECKS IN THE TABLE IF THE MEMBER IS LISTED
+                    const isOnTheListFirstMember = await members.isMemberOnTheList(member);
+                    await navigation.screenshot("members-deletion");
+
+                    // ASSERTION - MEMBER IS LISTED
+                    expect(isOnTheListFirstMember.found).toBe(true);
+
+                    await page.locator(".view-actions-top-row div[data-test-button='members-filter-actions']").click();
+                    await navigation.screenshot("members-deletion");
+                    await page.waitForLoadState("domcontentloaded");
+                    await page.waitForSelector(".gh-filters");
+
+                    
+                    await page.locator(".gh-filters select[data-test-select='members-filter']").first().selectOption("email");
+
+
+                    /*await page.locator("input[data-test-input='members-filter-value']").first().fill(member.fullname);
+                    await navigation.screenshot("members-deletion--aaaaa");
+                    await page.locator("button[data-test-button='members-apply-filter']").click();
+                    await page.waitForSelector("table.gh-list");
+
+                    
+                    const isOnTheListMember = await members.isMemberOnTheList(member);
+                    expect(isOnTheListMember.found).toBe(true); */
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
